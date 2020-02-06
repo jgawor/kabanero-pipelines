41c0e6d6cad1b4a520cf2e176f75612d4a9120e2
Jarek Gawor <you@example.com>
2020-02-06 16:21:01 -0500

    use full path

--- ci/nginx/startup.sh
+++ ci/nginx/startup.sh
@@ -2,7 +2,7 @@
 
 CONF_FILE=
 if [ -f "/etc/tls/private/tls.crt" -a -f  "/etc/tls/private/tls.key" ]; then
-    CONF_FILE="-c nginx-ssl.conf"
+    CONF_FILE="-c /etc/nginx/nginx-ssl.conf"
 fi
 
 exec nginx $CONF_FILE
